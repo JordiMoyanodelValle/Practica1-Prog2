af69d9fac0c425f84ab6f5806e39d36e8358df42
JordiMoyanodelValle <jmoyande15@alumnes.ub.edu>
2025-03-04 13:34:32 +0100

    Casa , bungalow, bungalow premium, client , parcela, reserva etc...

--- src/prog2/model/MobilHome.java
+++ src/prog2/model/MobilHome.java
@@ -2,5 +2,8 @@ package prog2.model;
 
 public class MobilHome extends Casa{
     private boolean terrasaAmbBarbacoa;
-    public MobilHome(String nom, String id,  boolean terrasaAmbBarbacoa) {}
+    public MobilHome(String nom, String id, String mida, int habitacions, int capacitat, boolean terrasaAmbBarbacoa) {
+        super(nom, id, mida, habitacions, capacitat);
+        this.terrasaAmbBarbacoa = terrasaAmbBarbacoa;
+    }
 }
